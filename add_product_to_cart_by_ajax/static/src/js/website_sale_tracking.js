35824f853a8effd447e1460247f056b67a870129
HauTran <duchau00212@gmail.com>
2016-10-18 11:16:55 +0700

    test

--- add_product_to_cart_by_ajax/static/src/js/website_sale_tracking.js
+++ add_product_to_cart_by_ajax/static/src/js/website_sale_tracking.js
@@ -14,45 +14,7 @@ odoo.define('add_product_to_cart_by_ajax.tracking', function(require) {
                 $("sup.my_cart_quantity").parent().parent().removeClass('hidden');
             });
         });
-          
-          
-          
-          
-          
-          
-          
-          
-          ////////////
-          
-        $(".oe_website_sale form[action='/shop/cart/update'] a.a-submit-ajax").on('click2', function(o) {
-            var self = $(this);
-            self.addClass('hidden');
-            var prod_id = self.parent().children("input[name='product_id']").attr('value');
-            var product_ids = [prod_id]; 
-            ajax.jsonRpc("/shop/cart/update_json", 'call', { 
-                'product_id': prod_id,
-                'add_qty': 1
-            }).then(function (data) {
-                console.log("Datas::" + JSON.stringify( data) );
-                var $q = $(".my_cart_quantity");
-                if (data.cart_quantity) {
-                    $q.parents('li:first').removeClass("hidden");
-                }
-                else {
-                    $q.parents('li:first').addClass("hidden");
-                    $('a[href^="/shop/checkout"]').addClass("hidden");
-                }
-                $q.html(data.cart_quantity).hide().fadeIn(600);
-            }); 
-             
-        });
-          
-               
-          
-          
-          
-          
-          //////////////
+            
           
           
           
